5e9a2ec5d4d536b9a17b92d15de8a344851238fb
SRS30 <106657748+SRS30@users.noreply.github.com>
2023-03-13 15:25:27 +0545

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutterquiz/app/app.dart';
+
+//Hamro Quiz - v-2.0.3
+
+void main() async {
+
+  runApp(await initializeApp());
+
+}
---
+++ lib/ui/styles/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+final Color primaryColor = Color(0xfff60808);
+final Color secondaryColor = Color(0xff434B5D); //work as secondary color
+final Color backgroundColor = Colors.white;
+final Color pageBackgroundColor = Color(0xFFFFDDDD);
+final Color canvasColor = Color(0xffffdede);
+
+final Color darkPrimaryColor = Color(0xfffa814e);
+final Color darkSecondaryColor = Colors.white; //work as secondary color
+final Color darkBackgroundColor = Color(0xff393939);
+final Color darkPageBackgroundColor = Color(0xFF3D3D3D);
+final Color darkCanvasColor = Color(0xff5A5A5A);
+
+//it will be same for both light and dark theme
+final Color badgeLockedColor = Colors.grey;
+final Color addCoinColor = Colors.green;
+final Color hurryUpTimerColor = Colors.red;//timer color when 25% time left
+
